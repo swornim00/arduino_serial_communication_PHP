06787461147f6ca5e31fb322b0a1f0d8077ce99a
Swornim Shrestha <srestaswrnm@gmail.com>
2016-09-02 22:14:04 +0545

    Update control.php

--- control.php
+++ control.php
@@ -1,8 +1,8 @@
 <html>
 <form id="container">
 
-<button onclick="submit()" class="btn" name="ledVal" value="1" >On</button>
-<button  onclick="submit()" class="btn" name="ledVal" value="2">Off</button>
+<button class="btn" name="ledVal" value="1" >On</button>
+<button class="btn" name="ledVal" value="2">Off</button>
 </form>
 </html>
 <style>
@@ -27,7 +27,7 @@
 if(isset($_GET['ledVal'])){ //Check if there is any get request
 	$val = $_GET['ledVal']; 
 $fp = fopen("com3","w"); //opening the Serial port in write mode you can use fopen("com3","r+") if you want read and write 
-
+			// /dev/ttyACM0 or something like this for linux	
 	fwrite($fp,$val); //writing to serial port
 
 fclose($fp); 
